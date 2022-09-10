6fc39a11c0f56a843d55c5856ef93a5b9f9652c8
Mansij Maharjan <71772378+mansijmaharzn@users.noreply.github.com>
2022-09-10 10:40:09 +0545

    initial

---
+++ Practice for Exam/Pre-Boards/Set A/9.c
@@ -0,0 +1,16 @@
+/* WAP to create a file, write some contains in a file and
+display that contains in a console. */
+#include<stdio.h>
+
+int main() {
+    FILE *file;
+    char ch[100] = "Hello guys";
+
+    file = fopen("forNum9.txt", "w");
+
+    fputs(ch, file);
+
+    printf("Contains: %s", ch);
+
+    return 0;
+}
