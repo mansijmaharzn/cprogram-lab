e8e2af2f2163d77985c2076aadd3d504309a3b9e
Mansij Maharjan <71772378+mansijmaharzn@users.noreply.github.com>
2022-09-11 08:15:37 +0545

    solved

--- Practice for Exam/2077/7.c
+++ Practice for Exam/2077/7.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter range(n): ");
     scanf("%d", &n);
 
-    for (i=1; i<=n; i++) { // starting from 2 cuz 0 and 1 are non-prime numbers
+    for (i=2; i<=n; i++) { // starting from 2 cuz 0 and 1 are non-prime numbers
         flag = 0;
         for (j=1; j<=n; j++) {
             if (i%j==0) // not prime number
