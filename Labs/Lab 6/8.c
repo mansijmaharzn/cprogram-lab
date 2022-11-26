d3bdfb5f02d51a5cc5f78d6bee47e93518c6f611
Mansij Maharjan <71772378+mansijmaharzn@users.noreply.github.com>
2022-11-26 19:09:56 +0545

    solved
    paila ko wrong raixa

--- Labs/Lab 6/8.c
+++ Labs/Lab 6/8.c
@@ -4,44 +4,41 @@
 #include<stdio.h>
 
 int main() {
-    int a[3][3], b[3][3], c[3];
-    int i, j, k, sum;
-    int row=3, column=3;
+    int i, j, k;
+    int a[3][3], b[3][3], mul[3][3];
 
-    // Inputs
-    printf("For FIRST Matrix:\n");
-    for (i=0; i < row; i++) {
-        printf("For number %d row: \n", i+1);
-        for (j=0; j < column; j++) {
-            printf("For number %d column: ", j+1);
+    printf("Enter elements of first matrix:\n");
+    for(i=0;i< 3;i++){
+        for(j=0;j< 3;j++){
+            printf("a[%d][%d]: ",i,j);
             scanf("%d", &a[i][j]);
         }
     }
-
-    printf("For SECOND Matrix:\n");
-    for (i=0; i < row; i++) {
-        printf("For number %d row: \n", i+1);
-        for (j=0; j < column; j++) {
-            printf("For number %d column: ", j+1);
+ 
+    printf("Enter elements of second matrix:\n");
+    for(i=0;i< 3;i++){
+        for(j=0;j< 3;j++){
+            printf("b[%d][%d]: ",i,j);
             scanf("%d", &b[i][j]);
         }
     }
-
-    // Multiplication
-    for (i=0; i < row; i++) {
-        for (j=0; j < column; j++) {
-            sum = 0;
-            for (k=0; k < column; k++) {
-                sum += a[i][k] * b[k][i];
-            }
-            c[i] = sum;
-        }
+ 
+    for(i=0;i< 3;i++){
+        for(j=0;j< 3;j++){
+            mul[i][j] = 0;
+            for(k=0;k<3;k++){
+                mul[i][j] = mul[i][j] + a[i][k]*b[k][j];
+            } // 0+3*7+12*11+4*6
+        }     // 0+3*3+12*9+4*8
+              // 0+3*8+12*5+4*4
     }
-
-    // Output
-    printf("Results: \n");
-    for (i=0; i < row; i++) {
-        printf("%d \n",c[i]);
+    
+    printf("Multiplied matrix is:\n");
+    for(i=0;i< 3;i++){
+        for(j=0;j< 3;j++){
+            printf("%d\t", mul[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
